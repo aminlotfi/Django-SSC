cc7d237db672c86592180dd1947a1e91d8dedae3
Mohamadreza Kariminejad <mokar2001@gmail.com>
2021-03-03 22:40:51 +0330

    Feature 3 added | BackEnd

--- edu/urls.py
+++ edu/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Index, SignUp
+from .views import Index, SignUp, SignIn
 
 urlpatterns = [
     path('index/', Index.as_view(), name="index"),
-    path('signup/', SignUp.as_view(), name="signup")
+    path('signup/', SignUp.as_view(), name="signup"),
+    path('signin/', SignIn.as_view(), name="signin")
+
 ]
--- edu/views.py
+++ edu/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse
 
 class Index(View):
     def get(self, request):
-        return render(request, 'edu/index.html')
+        return render(request, 'edu/index.html', {'access': False})
 
 class SignUp(View):
     def get(self, request):
@@ -22,3 +23,18 @@ class SignUp(View):
         user = User(**data)
         user.save()
         return HttpResponse('Registered Successfully')
+
+class SignIn(View):
+    def get(self, request):
+        return render(request, 'edu/signInForm.html', {'error': False})
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        has_access = bool(authenticate(username=username, password=password))
+        if has_access:
+            return render(request, 'edu/index.html', {'access': True})
+        else:
+            return render(request, 'edu/signInForm.html', {'error': True})
+
+
+        
